6992656c74615dc4d8f46476847bc314152b5056
arda mulya <edotank47@gmail.com>
2023-07-07 16:23:53 +0700

    create new bank project

---
+++ src/main/java/com/dobudobu/bankapp/Entity/Account.java
@@ -0,0 +1,21 @@
+package com.dobudobu.bankapp.Entity;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name = "tbl_account")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Account {
+
+    
+
+}
---
+++ src/main/java/com/dobudobu/bankapp/Entity/Customer.java
@@ -0,0 +1,43 @@
+package com.dobudobu.bankapp.Entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@Table(name = "tbl_customer")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Customer {
+
+    @Id
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+    @GeneratedValue(generator = "uuid")
+    private String id;
+
+    @Column(name = "first_name", length = 50, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 70)
+    private String lastName;
+
+    @Column(name = "tanggal_lahir")
+    private LocalDate tanggalLahir;
+
+    @Column(name = "alamat")
+    private String alamat;
+
+    @Column(name = "no_telp", length = 20, unique = true)
+    private String noTelp;
+
+    @Column(name = "email", unique = true)
+    private String email;
+}
